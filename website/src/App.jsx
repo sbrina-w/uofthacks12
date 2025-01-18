2265a28ac82368f079da7a6650b764d306155191
Ruby Lu <rubylu12345@gmail.com>
2025-01-18 17:36:58 -0500

    add resume upload

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -5,14 +5,21 @@ function App() {
   return (
     <div className="container">
       <div className="left">
-        <img src={antImage} alt="Ant" style={{ maxWidth: '400px', height: 'auto' }} />
+        <img src={antImage} alt="Ant" className="ant-image" />
       </div>
       <div className="right">
         <h1>Submit Job Application</h1>
-        <div>
-          <label htmlFor="name">Name: </label>
-          <input type="text" id="name" />
-        </div>
+        <form className="application-form">
+          <div className="form-group">
+            <label htmlFor="name">Name: </label>
+            <input type="text" id="name" placeholder="Enter your name" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="resume">Resume: </label>
+            <input type="file" id="resume" accept=".pdf,.doc,.docx" />
+          </div>
+          <button type="submit" className="submit-button">Submit</button>
+        </form>
       </div>
     </div>
   );
