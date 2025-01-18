859f1dd8cd4be4a366899bf6b4e3c4f83d64edba
Ruby Lu <rubylu12345@gmail.com>
2025-01-18 17:08:10 -0500

    update layout

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -3,16 +3,18 @@ import antImage from './assets/ant.png';
 
 function App() {
   return (
-    <>
-      <div style={{ textAlign: 'center', marginBottom: '20px' }}>
-        <img src={antImage} alt="Ant" style={{ maxWidth: '200px', height: 'auto' }} />
+    <div className="container">
+      <div className="left">
+        <img src={antImage} alt="Ant" style={{ maxWidth: '400px', height: 'auto' }} />
       </div>
-      <h1>Submit Job Application</h1>
-      <div>
-        <label htmlFor="name">Name: </label>
-        <input type="text" id="name" />
+      <div className="right">
+        <h1>Submit Job Application</h1>
+        <div>
+          <label htmlFor="name">Name: </label>
+          <input type="text" id="name" />
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
